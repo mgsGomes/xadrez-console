4408d274a3c53fc3c9cb228599ddb4c19b2bb2d9
Micael de Souza <micaelmgs@gmail.com>
2024-08-03 00:47:33 -0300

    - Melhorado a impressão do tabuleiro.

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -14,7 +14,11 @@ namespace xadrez_console
 
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(1, 9));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(6, 1));
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(2, 7));
 
                 Tela.imprimirTabuleiro(tab);
             }
