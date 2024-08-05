92d6ba5e091be57c607481d164d64a97f220d568
Micael de Souza <micaelmgs@gmail.com>
2024-08-05 15:24:24 -0300

    - Realizado os primeiros testes de movimentação

--- xadrez-console/Tabuleiro/Peca.cs
+++ xadrez-console/Tabuleiro/Peca.cs
@@ -20,5 +20,11 @@ namespace tabuleiro
             this.qteMovimentos = 0;
             this.tab = tab;
         }        
+
+        public void incrementarQteMovimentos() 
+        {
+            qteMovimentos++;
+
+        }
     }
 }
--- xadrez-console/Tabuleiro/Tabuleiro.cs
+++ xadrez-console/Tabuleiro/Tabuleiro.cs
@@ -46,6 +46,19 @@ namespace tabuleiro
             p.posicao = pos;
         }
 
+        public Peca removerPeca(Posicao pos)
+        {
+            if (peca(pos) == null)
+            {
+                return null;
+            }
+
+            Peca aux = peca(pos);
+            aux.posicao = null;
+            pecas[pos.linha, pos.coluna] = null;
+            return aux;
+        }
+
         public bool posicaoValida(Posicao pos)
         {
             if (pos.linha < 0 || pos.linha >= linhas || pos.coluna < 0 || pos.coluna >= colunas)
